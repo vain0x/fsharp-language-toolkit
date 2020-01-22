b58ad1975068bc7b69a94335fce73b555bf4c11a
vain0x <vainzerox@gmail.com>
2020-01-23 00:33:55 +0900

    feat: More LSP types

--- fltk-lsp/LspTypes.fs
+++ fltk-lsp/LspTypes.fs
@@ -2,8 +2,13 @@ namespace rec FSharpLanguageToolKit.LspTypes
 
 open System
 
+// FIXME: JsonValue?
+type Any = obj
+
 type DocumentUri = string
 
+// NOTE: Not marked as struct because struct unions can't be deserialized
+//       with Utf8Json (as far as I know).
 [<RequireQualifiedAccess>]
 type NumberOrString =
     | Number
@@ -49,15 +54,10 @@ type Diagnostic =
     {
         Range: Range
         Severity: Nullable<DiagnosticSeverity>
-
         Code: Option<NumberOrString>
-
         Source: Option<string>
-
         Message: string
-
         Tags: Option<DiagnosticTag[]>
-
         RelatedInformation: Option<DiagnosticRelatedInformation[]>
     }
 
@@ -77,8 +77,88 @@ type DiagnosticTag =
 type DiagnosticRelatedInformation =
     {
         Location: Location
-
         Message: string
     }
 
-// next: Command
+[<CLIMutable>]
+type Command<'TArguments> =
+    {
+        Title: string
+        Command: string
+        Arguments: Option<'TArguments>
+    }
+
+[<CLIMutable>]
+type TextEdit =
+    {
+        Range: Range
+        NewText: string
+    }
+
+[<CLIMutable>]
+type TextDocumentEdit =
+    {
+        TextDocument: VersionedTextDocumentIdentifier
+        Edits: TextEdit[]
+    }
+
+[<CLIMutable>]
+type CreateFileOptions =
+    {
+        Overwrite: Option<bool>
+        IgnoreIfExists: Option<bool>
+    }
+
+[<CLIMutable>]
+type CreateFile =
+    {
+        Kind: string
+        Uri: DocumentUri
+        Options: Option<CreateFileOptions>
+    }
+
+[<CLIMutable>]
+type RenameFileOptions =
+    {
+        Kind: string
+        OldUri: DocumentUri
+        NewUri: DocumentUri
+        Options: Option<RenameFileOptions>
+    }
+
+[<CLIMutable>]
+type DeleteFileOptions =
+    {
+        Recursive: Option<bool>
+        IgnoreIfNotExists: Option<bool>
+    }
+
+[<CLIMutable>]
+type DeleteFile =
+    {
+        Kind: string
+        Uri: DocumentUri
+        Options: Option<DeleteFileOptions>
+    }
+
+// next: WorkspaceEdit
+
+
+
+
+
+
+
+[<CLIMutable>]
+[<Struct>]
+type TextDocumentIdentifier =
+    {
+        Uri: DocumentUri
+    }
+
+[<Struct>]
+type VersionedTextDocumentIdentifier =
+    {
+        Uri: DocumentUri
+        Version: Option<int>
+    }
