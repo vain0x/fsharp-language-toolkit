1524cfe2c77839ef826127a838fb09afcb74400e
vain0x <vainzerox@gmail.com>
2020-01-22 22:27:16 +0900

    feat: Use generic type for LSP requests

--- fltk-lsp/LspRequests.fs
+++ fltk-lsp/LspRequests.fs
@@ -3,6 +3,20 @@
 open System
 
 [<CLIMutable>]
+type RequestMessage<'TParam> =
+    {
+        Jsonrpc: string
+
+        // FIXME: Should accommodate strings.
+        Id: int64
+
+        Method: string
+
+        /// nullable
+        Params: 'TParam
+    }
+
+[<CLIMutable>]
 type InitializeParams =
     {
         ProcessId: Nullable<int64>
@@ -10,7 +24,4 @@ type InitializeParams =
 
 [<CLIMutable>]
 type InitializeRequest =
-    {
-        Id: int64
-        Params: InitializeParams
-    }
+    RequestMessage<InitializeParams>
